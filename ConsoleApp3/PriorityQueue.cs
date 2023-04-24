2d3c5d665fa9be9df1d3a71c1d648cdf30d33f62
WOO YOUNG PARK <dhdhdud1@naver.com>
2023-04-24 21:36:30 +0900

    초기 커밋.

---
+++ ConsoleApp3/PriorityQueue.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class PriorityQueue<TElement>
+    {
+        private struct Node
+        {
+            public TElement element;
+            public int priority;
+        }
+
+        private List<Node> nodes;
+        // 구조체 Node를 List에 넣어줌
+
+        public PriorityQueue()
+        {
+            this.nodes = new List<Node>();
+        }
+        public int Count { get { return nodes.Count; } }
+        // nodes의 요소값을 반환
+
+        public void Enqueue(TElement element, int priority)
+        {
+            Node newNode = new Node() { element = element, priority = priority };
+
+            nodes.Add(newNode);
+            int newNodeIndex = nodes.Count - 1;
+            // newNodeIndex는 노드의 배열값에서 1을 감소함
+
+            while (newNodeIndex > 0)
+            // 새로운 노드를 힙상태가 유지되도록 승격 작업 반복
+            {
+                int parentIndex = GetParentIndex(newNodeIndex);
+                Node parentNode = nodes[parentIndex];
+
+                if (newNode.priority < parentNode.priority)
+                {
+                    nodes[newNodeIndex] = parentNode;
+                    nodes[parentIndex] = newNode;
+                    newNodeIndex = parentIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        public TElement Dequeue()
+        {
+            Node rootNode = nodes[0];
+            Node lastNode = nodes[nodes.Count - 1];
+            nodes[0] = lastNode;
+            nodes.RemoveAt(nodes.Count - 1);
+
+            int index = 0;
+            while (index < nodes.Count)
+            {
+                int lefrChildIndex = GetLeftChildIndex(index);
+                int rightChildIndex = GetRightChildIndex(index);
+
+                if (rightChildIndex < nodes.Count)
+                {
+                    int lessChildIndex = nodes[lefrChildIndex].priority < nodes[rightChildIndex].priority
+                        ? lefrChildIndex : rightChildIndex;
+
+                    if (nodes[lessChildIndex].priority < nodes[index].priority)
+                    {
+                        nodes[index] = nodes[lefrChildIndex];
+                        nodes[lessChildIndex] = lastNode;
+                        index = lessChildIndex;
+                    }
+                }
+                else if (lefrChildIndex < nodes.Count)
+                {
+                    if (nodes[lefrChildIndex].priority < nodes[index].priority)
+                    {
+                        nodes[index] = nodes[lefrChildIndex];
+                        nodes[lefrChildIndex] = lastNode;
+                        index = lefrChildIndex;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return rootNode.element;
+        }
+        private int GetParentIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+        public TElement Peek()
+        {
+            return nodes[0].element;
+        }
+        public int GetLeftChildIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+        private int GetRightChildIndex(int childIndex)
+        {
+            return (childIndex - 1) / 2;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace PriorityQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<string, int> queue = new PriorityQueue<string, int>();
+            // 노드의 이름과 우선순위를 새로 선언
+
+            queue.Enqueue("앞", 10);
+            queue.Enqueue("앞앞", 20);
+            queue.Enqueue("앞앞앞", 30);
+            queue.Enqueue("앞앞앞앞", 40);
+            queue.Enqueue("앞앞앞앞앞", 50);
+            queue.Enqueue("앞앞앞앞앞앞", 60);
+            queue.Enqueue("앞앞앞앞앞앞앞", 70);
+            queue.Enqueue("앞앞앞앞앞앞앞앞", 80);
+            queue.Enqueue("앞앞앞앞앞앞앞", 90);
+            queue.Enqueue("앞앞앞앞앞앞", 100);
+            queue.Enqueue("앞앞앞앞앞", 110);
+            queue.Enqueue("앞앞앞앞", 120);
+            queue.Enqueue("앞앞앞", 130);
+            queue.Enqueue("앞앞", 140);
+            queue.Enqueue("앞", 150);
+            queue.Enqueue("뒤", 9);
+            queue.Dequeue();
+
+
+
+            while (queue.Count > 0)
+            {
+
+                Console.WriteLine(queue.Dequeue());
+                Console.WriteLine(queue.Dequeue());
+
+            }
+        }
+    }
+}
